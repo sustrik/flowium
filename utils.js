3a8778be6056b6faafd2a623c209539d1979f265
Martin Sustrik <sustrik@250bpm.com>
2018-07-21 07:43:10 +0200

    getToken function
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- utils.js
+++ utils.js
@@ -32,3 +32,44 @@ var HttpClient = function() {
     }
 }
 
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
+function getToken() {
+    token = getCookie("flowium")
+    if(token != null) return token
+    console.log("Setting cookie")
+    var code = getParameterByName('code')
+    if(code == null) {
+        window.location.replace("https://github.com/login/oauth/authorize?" +
+            "scope=repo&client_id=d027578d9cca180f9e0e")
+        return
+    }
+    var client = new HttpClient();
+    client.get("https://flowium.herokuapp.com/authenticate/" + code,
+          function(response) {
+        console.log(response)
+    })
+    return "test"
+}
+
