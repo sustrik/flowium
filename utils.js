e0a62565fdbf343ba8a52ed51bafc85e2e31b02a
Martin Sustrik <sustrik@250bpm.com>
2018-08-23 08:43:24 +0200

    Gitea
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- utils.js
+++ utils.js
@@ -420,7 +420,7 @@ function gtGet(path, args, cb) {
             }
         }
     }
-    request.open('GET', flowiumBackend.root + "/api/v1/" + path, true);
+    request.open('GET', flowiumBackend.URL + "/api/v1/" + path, true);
     request.setRequestHeader('Authorization','token ' +
         flowiumBackend.token)
     request.setRequestHeader('Content-type', 'application/json')
